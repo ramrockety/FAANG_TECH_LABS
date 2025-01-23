47ff89774cb3e380a999d68b639aebd8884353da
Rama Brahmam <36239904+ramrockety@users.noreply.github.com>
2025-01-23 01:56:09 -0500

    Update PascalTriangle.java

--- Arrays/PascalTriangle.java
+++ Arrays/PascalTriangle.java
@@ -1,3 +1,21 @@
+/*
+----problem statement-----
+
+Given an integer numRows return the first numRows rows of Pascal's triangle.
+
+In Pascal's triangle:
+﻿﻿The first row has one element with a value of 1.
+﻿﻿Each row has one more element in it than its previous row.
+﻿﻿The value of each element is equal to the sum of the elements directly above it when arranged in a triangle format.
+
+Examples:
+Input: numRows = 4
+Output: (11, (1, 1L (1, 2, 1L, (1, 3, 3, 1])
+Explanation: 1st Row has its value set to 1.
+All other cells take their value as the sum of the values directly above them
+
+*/
+
 import java.util.*;
 public class PascalTriangle
 {
